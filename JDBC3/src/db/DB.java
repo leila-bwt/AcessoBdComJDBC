7b1ef86e238a1765207aee92bfdabdfa9b5deb4f
leila.bwt <leila.bwt@gmail.com>
2024-01-26 21:14:31 -0300

    Inserir Dados

--- JDBC3/src/db/DB.java
+++ JDBC3/src/db/DB.java
@@ -4,48 +4,76 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
-
-    private static Connection conn = null;
-
-    public static Connection getConnection(){
-        if(conn == null){
-            try{
-            Properties props = loadProperties();
-            String url = props.getProperty("dburl");
-            conn = DriverManager.getConnection(url, props);
-        }
-        catch (SQLException e){
-            throw new DbException(e.getMessage());
-        }
-    }
-    return conn;
-}
-
-    public static void closeConnection(){
-        if(conn != null){
-            try{
-                conn.close();
-            }
-            catch(SQLException e){
-                throw new DbException(e.getMessage());
-            }
-        }
-    }
-
-    //carrega as propriedades do banco em db.properties
-    private static Properties loadProperties(){
-        try (FileInputStream fs = new FileInputStream("db.properties")){
-            Properties props = new Properties();
-            props.load(fs);
-            return props;
-        }
-        catch (IOException e){
-            throw new DbException(e.getMessage());
-        }
-    }
-    
+	
+	//gerar conexão com banco de dados JDBC
+	
+	private static Connection conn = null;
+	
+	public static Connection getConnection() {
+		if(conn == null) {
+			try {
+				Properties props = LoadProperties();
+				String url = props.getProperty("dburl");
+				conn = DriverManager.getConnection(url, props);
+			}
+			catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		return conn;
+	}
+	
+	//Fechar conexão com o banco
+	public static void closeConnection() {
+		if (conn != null) {
+			try {
+				conn.close();
+			}
+			catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+	
+	
+	// chama os dados do banco
+	private static Properties LoadProperties() {
+		try(FileInputStream fs = new FileInputStream("db.properties")){
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+		}
+		catch (IOException e) {
+			throw new DbException(e.getMessage());
+		}
+		
+	}
+	
+	// fechar um objeto do tipo Statement -- evitar vazamento de memória
+	
+	public static void closeStatement(Statement st) {
+		if (st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	public static void closeResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
 }
